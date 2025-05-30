d12ee74cd24122b6d2307039f101c65c3191e6f5
Gustavo de Madruga Maciel <126895105+gstvmadruga@users.noreply.github.com>
2025-05-30 00:04:53 -0300

    Create ContaBancaria.java

---
+++ criacao-de-classes/Exercicio01/dominio/ContaBancaria.java
@@ -0,0 +1,40 @@
+package contabancaria;
+
+public class ContaBancaria {
+    private String titular;
+    private double saldo;
+
+    public ContaBancaria(String titular) {
+        this.titular = titular;
+        this.saldo = 0.0; // Saldo inicial zero
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+            System.out.println("Depósito de R$ " + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Valor de depósito inválido.");
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0 && valor <= this.saldo) {
+            this.saldo -= valor;
+            System.out.println("Saque de R$ " + valor + " realizado com sucesso.");
+        } else if (valor <= 0) {
+            System.out.println("Valor de saque inválido.");
+        } else {
+            System.out.println("Saldo insuficiente para saque.");
+        }
+    }
+
+    public void exibirSaldo() {
+        System.out.println("Titular: " + this.titular);
+        System.out.println("Saldo atual: R$ " + this.saldo);
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+}
