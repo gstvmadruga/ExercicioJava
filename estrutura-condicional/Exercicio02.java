520d9f70b8518c34456b30a93900658346b87c99
Gustavo de Madruga Maciel <126895105+gstvmadruga@users.noreply.github.com>
2025-05-29 23:46:11 -0300

    Create Exercicio02.java

---
+++ estrutura-condicional/Exercicio02.java
@@ -0,0 +1,22 @@
+package ClassificaIdade;
+
+import java.util.Scanner;
+
+public class ClassificaIdade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite a idade: ");
+        int idade = scanner.nextInt();
+        
+        if (idade < 18) {
+            System.out.println("Menor de idade");
+        } else if (idade <= 60) {
+            System.out.println("Adulto");
+        } else {
+            System.out.println("Idoso");
+        }
+        
+        scanner.close();
+    }
+}
