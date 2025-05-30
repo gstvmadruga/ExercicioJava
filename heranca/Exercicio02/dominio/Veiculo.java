13ad6e2676aa996ca7e6f18719ba33d26cbba1ce
Gustavo de Madruga Maciel <126895105+gstvmadruga@users.noreply.github.com>
2025-05-30 00:25:03 -0300

    Create Veiculo.java

---
+++ heranca/Exercicio02/dominio/Veiculo.java
@@ -0,0 +1,31 @@
+package dominio;
+
+public class Veiculo {
+    protected String marca;
+    protected String modelo;
+    protected int ano;
+
+    public Veiculo(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public void exibirDados() {
+        System.out.println("Marca: " + this.marca);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Ano: " + this.ano);
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+}
