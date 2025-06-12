ef69e2363a2f6322453fe36c31ae1195e2a41539
Gustavo de Madruga Maciel <126895105+gstvmadruga@users.noreply.github.com>
2025-06-12 16:03:13 -0300

    Create Main.java

---
+++ manipulacao-de-arquivos/exercicios/Exercicio01/view/Main.java
@@ -0,0 +1,8 @@
+import javafx.application.Application;
+import view.TextoView;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(TextoView.class);
+    }
+}
