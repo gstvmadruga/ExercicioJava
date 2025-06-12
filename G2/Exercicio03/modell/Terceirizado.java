738b2c543f7acdbf76670fec80da244d9c4a08f7
Gustavo de Madruga Maciel <126895105+gstvmadruga@users.noreply.github.com>
2025-06-12 20:24:21 -0300

    Update Terceirizado.java

--- G2/Exercicio03/modell/Terceirizado.java
+++ G2/Exercicio03/modell/Terceirizado.java
@@ -1,1 +1,30 @@
+package model;
+
+public class Terceirizado extends Funcionario {
+    private double valorContrato;
+
+    public Terceirizado(String nome, int matricula, double valorContrato) {
+        super(nome, matricula);
+        this.valorContrato = valorContrato;
+    }
+
+    public double getValorContrato() {
+        return valorContrato;
+    }
+
+    public void setValorContrato(double valorContrato) {
+        this.valorContrato = valorContrato;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return valorContrato;
+    }
+
+    @Override
+    public void exibir() {
+        System.out.println("Funcionário Terceirizado:");
+        super.exibir();
+    }
+}
 
