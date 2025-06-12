5d1943843b71211b93f8c6dc5c4f22bdd89f47f9
Gustavo de Madruga Maciel <126895105+gstvmadruga@users.noreply.github.com>
2025-06-12 16:06:07 -0300

    Create MensagemModel.java

---
+++ manipulacao-de-arquivos/exercicios/Exercicio03/model/MensagemModel.java
@@ -0,0 +1,13 @@
+package model;
+
+public class MensagemModel {
+    private String mensagem;
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+}
