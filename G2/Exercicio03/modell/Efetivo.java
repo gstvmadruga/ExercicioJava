bf35a49e066faf661ccd1aa8bcfdf713f93d4ffc
Gustavo de Madruga Maciel <126895105+gstvmadruga@users.noreply.github.com>
2025-06-12 20:23:54 -0300

    Update Efetivo.java

--- G2/Exercicio03/modell/Efetivo.java
+++ G2/Exercicio03/modell/Efetivo.java
@@ -1,20 +1,30 @@
 package model;
 
 public class Efetivo extends Funcionario {
-    private double salario;
+    private double salarioBase;
 
-    public Efetivo(String nome, String matricula, double salario) {
+    public Efetivo(String nome, int matricula, double salarioBase) {
         super(nome, matricula);
-        this.salario = salario;
+        this.salarioBase = salarioBase;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
     }
 
     @Override
-    public String exibirDados() {
-        return "Efetivo - Nome: " + nome + ", Matrícula: " + matricula + ", Salário: " + salario;
+    public double calcularSalario() {
+        return salarioBase;
     }
 
     @Override
-    public void atualizarDados(String... dados) {
-        this.salario = Double.parseDouble(dados[0]);
+    public void exibir() {
+        System.out.println("Funcionário Efetivo:");
+        super.exibir();
     }
 }
+
