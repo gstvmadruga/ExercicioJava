b2995420908cdf269d092607fc02924eb6e2f32d
Gustavo de Madruga Maciel <126895105+gstvmadruga@users.noreply.github.com>
2025-06-12 20:24:09 -0300

    Update Temporario.java

--- G2/Exercicio03/modell/Temporario.java
+++ G2/Exercicio03/modell/Temporario.java
@@ -1,1 +1,40 @@
+package model;
+
+public class Temporario extends Funcionario {
+    private double valorHora;
+    private int horasTrabalhadas;
+
+    public Temporario(String nome, int matricula, double valorHora, int horasTrabalhadas) {
+        super(nome, matricula);
+        this.valorHora = valorHora;
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+
+    public double getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    public int getHorasTrabalhadas() {
+        return horasTrabalhadas;
+    }
+
+    public void setHorasTrabalhadas(int horasTrabalhadas) {
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return valorHora * horasTrabalhadas;
+    }
+
+    @Override
+    public void exibir() {
+        System.out.println("Funcionário Temporário:");
+        super.exibir();
+    }
+}
 
