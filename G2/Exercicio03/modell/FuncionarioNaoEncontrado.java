c24d6cbfeeed324020a61cbbf4ab565ba23b19b4
Gustavo de Madruga Maciel <126895105+gstvmadruga@users.noreply.github.com>
2025-06-12 21:55:11 -0300

    Update FuncionarioNaoEncontrado.java

--- G2/Exercicio03/modell/FuncionarioNaoEncontrado.java
+++ G2/Exercicio03/modell/FuncionarioNaoEncontrado.java
@@ -1,8 +1,8 @@
 package model;
 
 public class FuncionarioNaoEncontradoException extends Exception {
-    public FuncionarioNaoEncontradoException(String mensagem) {
-        super(mensagem);
+    public FuncionarioNaoEncontradoException(String message) {
+        super(message);
     }
 }
 
